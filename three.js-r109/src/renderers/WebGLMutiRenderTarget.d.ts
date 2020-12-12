49cac1239c5e87754bd39011adeacf5b83fe10b3
comma <comma@zju.edu.cn>
2020-12-12 15:39:05 +0800

    init

---
+++ three.js-r109/src/renderers/WebGLMutiRenderTarget.d.ts
@@ -0,0 +1,14 @@
+import {
+	WebGLRenderTarget,
+	WebGLRenderTargetOptions,
+} from './WebGLRenderTarget';
+
+export class WebGLMultiRenderTarget extends WebGLRenderTarget {
+
+	constructor(
+		width: number,
+		height: number,
+		options?: WebGLRenderTargetOptions
+	);
+
+}
